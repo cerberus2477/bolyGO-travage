72f820a052f6c2ac26cca5291bcc26b5df3c8d23
Buzino <160595331+Buzino@users.noreply.github.com>
2024-02-22 12:00:54 +0000

    API fájl alapjai.

--- bolyGO-travage-oldal/backend/api.php
+++ bolyGO-travage-oldal/backend/api.php
@@ -1,3 +1,63 @@
 <?php
 
-echo "szeretet";
+header('Content-Type: application/json; charset=utf-8');
+
+switch($_SERVER["REQUEST_METHOD"]) {
+    case "GET":
+        if (isset($_GET["csomagid"])) getCsomag($_GET["csomagid"]);
+        else getCsomagok();
+        break;
+    case "POST":
+        foglal();
+        break;
+
+}
+
+//csomagok leírásának, nevének, azonosítójának lekérése
+function getCsomagok() {
+    $sql = "SELECT";
+    connectDB();
+    //*lekérés*
+    closeDB();
+}
+
+//egy csomag adatainak lekérése a csomagid alapján
+function getCsomag($csomagid) {
+    $sql = "SELECT";
+    connectDB();
+    //*lekérés*
+    closeDB();
+}
+
+//foglalások feltöltése az adatbázisba
+function foglal() {
+    connectDB();
+    //*lekérés*
+    closeDB();
+}
+
+//kapcsolódás az adatbázishoz
+function connectDB() {
+    try {
+        $adb = mysqli_connect( "localhost", "root", "", "bolygodb" );
+    } catch (Exception $e) {
+        hibauzenet($e);
+    }
+}
+
+//adatbáziskapcsolat bezárása
+function closeDB() {
+    try {
+        mysqli_close( $adb ) ;
+    } catch (Exception $e) {
+        hibauzenet($e);
+    }
+}
+
+//hibaüzenet visszaküldése
+function hibauzenet($e) {
+    $uzenet = array("hiba" => "SQL-hiba", "uzenet" => "Nem sikerült csatlakozni az adatbázishoz. Hibaüzenet: ".$e->getMessage());
+    $json = json_encode($uzenet);
+    echo $json;
+
+}
